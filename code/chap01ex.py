a4ad01b35dd337d08b38e381a5400c0ddcb683fc
Gabriel Perdue <gnperdue@me.com>
2014-10-26 21:39:39 -0500

    Start on ex 1.2.

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -3,15 +3,31 @@ by Allen B. Downey, available from greenteapress.com
 
 Copyright 2014 Allen B. Downey
 License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
+
+Modified and extended by G. Perdue.
 """
 
 from __future__ import print_function
 
-import numpy as np
+# import numpy as np
 import sys
 
-import nsfg
+# import nsfg
 import thinkstats2
+
+
+def ReadFemResp(dct_file='2002FemResp.dct',
+                dat_file='2002FemResp.dat.gz'):
+    """Reads the NSFG respondent data.
+
+    dct_file: string file name
+    dat_file: string file name
+
+    returns: DataFrame
+    """
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip')
+    return df
 
 
 def main(script):
